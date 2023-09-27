6f3827ea206c8f037fc512f2311753d3542ea1a5
meekachm <meeka.chm@gmail.com>
2023-09-26 23:39:03 -0700

    displayed cart items on console

--- scripts.js
+++ scripts.js
@@ -49,17 +49,52 @@ for (let i = 0; i < data.length; i += 1) {
 const cart = []
 
 function addItem(name, price) {
-    const item = { name: name, price: price, qty: 1 }
-    cart.push(name)
+	for (let i = 0; i < cart.length; i +=1) {
+		if (cart[i].name === name) {
+			cart[i].qty += 1
+			return
+		}
+	}
+    const item = { name, price, qty: 1 }
+    cart.push(item)
 }
 
+// Show Items
 function showItems() {
-    console.log(`You have ${cart.length} items in your cart.`)
+	const qty = getQty()
+	console.log(`You have ${qty} items in your cart`)
+
+	for (let i = 0; i < cart.length; i += 1) {
+		console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+	}
+
+	console.log(`Total in cart: $${getTotal()}`)
 }
 
-addItem('Happiness',  5.99)
+// Get Qty
+function getQty() {
+	let qty = 0
+    for (let i = 0; i < cart.length; i += 1) {
+		qty += cart[i].qty  
+    }
+	return qty
+}
+
+// Get total
+function getTotal() {
+	let total = 0
+	for (let i = 0; i < cart.length; i += 1) {
+		total += cart[i].price * cart[i].qty
+	}
+	return total.toFixed(2)
+}
+
+addItem('Happiness', 5.99)
 addItem('Anger', 5.99)
-addItem('Disgust', 5.99)
+addItem('Happiness', 5.99)
+addItem('Shy', 5.99)
+addItem('Sleep', 5.99)
+addItem('Anger', 5.99)
 addItem('Shy', 5.99)
 
 showItems()
